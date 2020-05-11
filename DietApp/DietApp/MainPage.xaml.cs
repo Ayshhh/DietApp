ea4f32d1ecf60f242c1557ebf8a0f4d576aa2f92
Ayshhh <Ayshhh@github.com>
2020-05-11 23:42:46 +0530

    Add project files.

---
+++ DietApp/DietApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DietApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DietApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DietList.ItemsSource = plan;
+
+        }
+
+        ObservableCollection<DietPlan> plan = new ObservableCollection<DietPlan>()
+        {
+        };
+
+        public void Add_Clicked(object sender, EventArgs e)
+        {
+            plan.Add(new DietPlan { FoodName = EFName.Text, Quantity = EQnty.Text, Calories = EClr.Text, Taste = ETasty.Text });
+        }
+    }
+}
---
+++ DietApp/DietApp/Model/DietPlan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DietApp.Model
+{
+    public class DietPlan
+    {
+        public string FoodName { get; set; }
+        public string Quantity { get; set; }
+        public string Calories { get; set; }
+        public string Taste { get; set; }
+    }
+}
